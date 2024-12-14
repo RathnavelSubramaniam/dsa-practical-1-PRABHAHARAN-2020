d752cb5c949ef96ce35a6811951da36884498be1
PRABHAHARAN A <122508061+PRABHAHARAN-2020@users.noreply.github.com>
2024-12-14 10:06:00 +0000

    CM1

--- stringReverse.java
+++ stringReverse.java
@@ -10,10 +10,15 @@ String str = input.nextLine();
 String reversed="";
 System.out.println("THE REVERSED STRING IS: " +reverseString(str,"",0,str.length()-1) );
 }
-public static String reverseString()
- {
+public static String reverseString(String str, String reversed, int startIndex, int endIndex) {
+        // Base Case
+           if (startIndex >endIndex) {
+            return reversed;
+        }
 
-//TYPE YOUR CODE HERE
-
- }
+        // Recursive Step
+        reversed += str.charAt(endIndex);
+        
+        return reverseString(str, reversed, startIndex, endIndex - 1);
+    }
 }
